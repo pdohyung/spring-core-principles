da048d86f082796c6313973438dfd6d02115c2e1
dohyung <pdohyung@gmail.com>
2023-02-06 04:17:13 +0900

    싱글톤 테스트 코드 구현

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -22,4 +22,18 @@ public class SingletonTest {
         // memberService1 != memberService2
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱클톤 패턴을 적용한 객체 사용")
+    void SingletonService(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1= " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+        // same ==
+        // equal 메소드
+    }
 }
