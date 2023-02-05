16615ad4c1012d9c3e5ed96272f3ccf021d2ffe8
dohyung <pdohyung@gmail.com>
2023-02-05 16:02:35 +0900

    스프링 컨테이너 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,35 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemServiceImpl;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean(name = "mmm")
     public MemberService memberService(){
-        return new MemServiceImpl(new MemoryMemberRepository());
+        return new MemServiceImpl(memberRepository());
     }  // 생성자 주입
-
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
-}
+}// 생성자 주입 리팩토링, new가 한번만 쓰이도록 코드를 수정했다.
