f2910c623cdfd569143a6731e81b83e6854f2de0
dohyung <pdohyung@gmail.com>
2023-02-11 19:25:22 +0900

    중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,15 +1,18 @@
 package hello.core;
 
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name="memoryMemberRepository")
+    MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
