09817fd945220a87e0149432f77157dff770d936
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-12-24 23:06:32 +0900

    initial commit

---
+++ src/main/java/project/core/SpringCorePrinciplesApplication.java
@@ -0,0 +1,13 @@
+package project.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCorePrinciplesApplication.class, args);
+    }
+
+}
