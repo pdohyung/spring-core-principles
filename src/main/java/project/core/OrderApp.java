a8f0388618740fa46de29bae44b4a18bef74b53b
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-01-12 22:17:58 +0900

    feat: Spring으로 전환 (#3)

--- src/main/java/project/core/OrderApp.java
+++ src/main/java/project/core/OrderApp.java
@@ -1,5 +1,7 @@
 package project.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import project.core.order.Order;
 import project.core.order.OrderService;
 import project.core.user.Grade;
@@ -9,15 +11,19 @@ import project.core.user.UserService;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        UserService userService = appConfig.userService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        UserService userService = appConfig.userService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long userId = 1L;
         User user = new User(userId, "userA", Grade.VIP);
         userService.join(user);
 
-        Order order = orderService.createOrder(userId, "itemA", 10_000);
+        Order order = orderService.createOrder(userId, "itemA", 20_000);
 
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
--- src/main/java/project/core/UserApp.java
+++ src/main/java/project/core/UserApp.java
@@ -1,12 +1,18 @@
 package project.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import project.core.user.*;
 
 public class UserApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        UserService userService = appConfig.userService();
+//        AppConfig appConfig = new AppConfig();
+//        UserService userService = appConfig.userService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+
         User user = new User(1L, "userA", Grade.VIP);
         userService.join(user);
 
